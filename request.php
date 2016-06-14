daaaca6be1e76cc3939b4922c8a49ee5285a91af
Domas <sungaila.d@gmail.com>
2016-06-14 13:03:25 +0300

    Pataisytas mail siuntimas.

--- request.php
+++ request.php
@@ -41,12 +41,12 @@ if ($valid) {
         "Reply-To: $email\r\n" .
         "X-Mailer: PHP/" . phpversion();
     $text = "El. paštas: $email\r\nVardas: $name\r\nTelefonas: $phone\r\nKlausimas:\r\n$message";
-    if (mail($primary_email, "digital.work klausimas", $text, $headers)) {
-        $success = true;
-        $alerts[] = 'Dėkojame už klausimą. Greitai su Jumis susisieksime.';
-    } else {
+    if (mail($primary_email, "digital.work klausimas", $text, $headers) === FALSE) {
         $success = false;
         $alerts[] = "Nepavyko išsiųsti Jūsų klausimo. Susisiekite su mumis el. paštu <a href='mailto:$primary_email'>$primary_email</a>.";
+    } else {
+        $success = true;
+        $alerts[] = 'Dėkojame už klausimą. Greitai su Jumis susisieksime.';
     }
 }
 
